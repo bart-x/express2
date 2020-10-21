d9078e8b3086015ba18b5c81ad744b81dedce7d1
Bartek K <kontener00@gmail.com>
2020-10-21 17:00:36 +0200

    add handlebars, style files

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+// app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-        res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.show('index.html');
+    res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about');
 });
 
-app.get('/user/settings', (req, res) => {
-    res.show('panel.html');
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info', { layout: 'dark' });
+});
+
+app.get('/history', (req, res) => {
+    res.render('history');
 });
 
 app.use((req, res) => {
-    res.status(404).show('404.jpg');
-});
+    res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
-}); 
+});
